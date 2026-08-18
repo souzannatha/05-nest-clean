5790b1bdc4fde94b00cc4097b96103dec371965d
souzannatha <souzanatha04@gmail.com>
2026-08-18 16:25:27 -0300

    chore(config): tighten lint and typescript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,30 @@
 import config from '@rocketseat/eslint-config/node.mjs'
 
-export default config
+const baseConfig = config
+
+const override = {
+  rules: {
+    '@stylistic/max-len': [
+      'error',
+      {
+        code: 150,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignorePattern: '^import\\s.+$',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: 'createAccountBodySchema',
+      },
+    ],
+  },
+}
+
+const finalConfig = Array.isArray(baseConfig)
+  ? [...baseConfig, override]
+  : [baseConfig, override]
+
+export default finalConfig
