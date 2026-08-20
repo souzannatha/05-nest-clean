08d24b9b436a539ec441caa77b406a07b2a1c37c
souzannatha <souzanatha04@gmail.com>
2026-08-20 17:03:50 -0300

    feat(app): configure server port from environment

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { Env } from './env.js'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
-  await app.listen(3333)
+
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  const port = configService.get('PORT', {
+    infer: true,
+  })
+
+  await app.listen(port)
 }
 bootstrap()
