f56c463873fa86ea651fa192bf1136fefb06520c
souzannatha <souzanatha04@gmail.com>
2026-08-20 17:03:46 -0300

    feat(config): add environment variable validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env.js'
 
 @Module({
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true,
+  })],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
