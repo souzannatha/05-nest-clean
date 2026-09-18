14f1d43af2b9ff5a5d7b3d0cdbe3e3f8c430934a
souzannatha <souzanatha04@gmail.com>
2026-09-17 21:40:32 -0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { PrismaService } from './prisma/prisma.service'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env.js'
+import { AuthModule } from './auth/auth.module.js'
 
 @Module({
   imports: [ConfigModule.forRoot({
     validate: env => envSchema.parse(env),
     isGlobal: true,
-  })],
+  }), AuthModule,
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
